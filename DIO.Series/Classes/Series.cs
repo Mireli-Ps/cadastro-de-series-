6fa47760fd0f7ab28f1889e02995a3ad3505461b
unknown <mireli.silva@fatec.sp.gov.br>
2021-04-16 21:08:48 -0300

    Colando pasta ja feita no vscode

---
+++ DIO.Series/Classes/Series.cs
@@ -0,0 +1,11 @@
+namespace DIO.Series
+{
+    public class Series : EntidadeBase
+    {
+        private Genero genero { get; set; }
+        private string Titulo { get; set; }
+        private string descricao{ get; set ;}
+        private int ano {get; set;}
+        
+    }
+}
---
+++ DIO.Series/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DIO.Series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Series meuobj = new Series();
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
